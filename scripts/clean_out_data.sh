4496ae925eba9e183efabe4312bcb1f3d80fa9a1
Evangelos <ediakatos@mapaction.org>
2024-04-30 10:58:30 +0000

    Extraction of osm and db logic from geocint-mapaction

--- scripts/clean_out_data.sh
+++ scripts/clean_out_data.sh
@@ -6,10 +6,6 @@
 
 # delete tables in DB
 for country_code in $(find data/out/country_extractions/ -mindepth 1 -maxdepth 1 -type d -printf '%f\n')
-do
-    psql -1 -c "DROP TABLE IF EXISTS osm_${country_code} cascade;"
-    psql -1 -c "DROP TABLE IF EXISTS mapaction_${country_code} cascade;"
-done
 
 # delete data/out
 rm -rf data/out
